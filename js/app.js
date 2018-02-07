fbe90297a30c2a59f11964e5e092c40af0765464
Matilda Carlson <a.matildacarlson@gmail.com>
2018-02-07 15:42:33 +0100

    Push push

--- js/app.js
+++ js/app.js
@@ -2,57 +2,36 @@ $(function() {
 	//We instantiate our model
 	var model = new DinnerModel();
 
-	var listener = function () {
+	var sidebarView = new SidebarView($("#sidebarView"), model);
+	var dishSearchView = new DishSearchView($("#dishSearchView"), model);
+	var dishDetailsView= new DishDetailsView($("#dishDetailsView"), model);
+	var topBarView = new TopBarView($("#topBarView"), model);
+	var dinnerOverviewView = new DinnerOverviewView($("#dinnerOverviewView"), model);
+	var dinnerPrintoutView = new DinnerPrintoutView($("#dinnerPrintoutView"), model);
+
+	document.getElementById("createDinnerButton").addEventListener("click", showSelectDishPage, false);
+	document.getElementById("createDinnerButton").addEventListener("click", showSelectDishPage, false);
+
+	// Index view
+	$('#sidebarView').hide();
+	$('#dishSearchView').hide();
+	$('#dishItemView').hide();
+	$('#indexView').show();
+	$('#dinnerOverviewView').hide();
+	$('#dinnerPrintoutView').hide();
+	$('#dishDetailsView').hide();
+	$('#topBarView').hide();
+
+	var showSelectDishPage = function () {
 	 	$('#sidebarView').show();
 	 	$('#dishSearchView').show();
 	 	$('#dishItemView').show();
 	 	$('#indexView').hide();
-	};
-
-//	var indexView = new IndexView($("#indexView"), model);
-	var sidebarView = new SidebarView($("#sidebarView"), model);
-	var dishSearchView = new DishSearchView($("#dishSearchView"), model);
-
-//	$('#indexView').show();
-	$('#sidebarView').hide();
-	$('#dishSearchView').hide();
-	$('#dishItemView').hide();
-
-	var btn = document.getElementById("createDinnerButton");
-	btn.addEventListener("click", listener, false);
-
-
-	
-	// And create the instance of ExampleView
-
-	/**if($("#allOfDishDetails").length){
-		var sidebarView = new SidebarView($("#sidebarView"), model);
-		var dishDetailsView= new DishDetailsView($("#dishDetailsView"), model);
-		$('#sidebarView').hide();
+	 	$('#dinnerOverviewView').hide();
+	 	$('#dinnerPrintoutView').hide();
 	 	$('#dishDetailsView').hide();
-	}
-
-	if($("#allOfDinnerOverview").length){
-		var topBarView = new TopBarView($("#topBarView"), model);
-		var dinnerOverviewView = new DinnerOverviewView($("#dinnerOverviewView"), model);
-	}
-
-	if($("#allOfSelectDish").length){
-		var sidebarView = new SidebarView($("#sidebarView"), model);
-		var dishSearchView = new DishSearchView($("#dishSearchView"), model)
-	}
-
-	if($("#allOfDinnerPrintout").length){
-		var topBarView = new TopBarView($("#topBarView"), model);
-		var dinnerPrintoutView = new DinnerPrintoutView($("#dinnerPrintoutView"), model);
-	}
-
-
-	if($("#allOfIndex").length){
-		var indexView = new IndexView($("#indexView"), model);
-
-	}/*
-//	var dinnerOverviewView = new DinnerOverviewView($("#dinnerOverviewView"), model);
+	 	$('#topBarView').hide();
+	};
 
 	/**
 	 * IMPORTANT: app.js is the only place where you are allowed to
--- js/view/indexView.js
+++
@@ -1,4 +0,0 @@
-var IndexView = function (container, model) { 
-
-
-}
